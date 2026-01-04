724f785566bf6f5f40d0874de57cdf8427a448ee
panashe12 <panashekanyai@gmail.com>
2026-01-04 22:38:45 +0200

    JJK

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,14 @@
-SHOW CREATE TABLE books;
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
